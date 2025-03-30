e4c8552137943d3af984c5a8f1ea702800e98ca9
Florent Azonnoudo <florent@weekshipper.com>
2025-03-30 00:22:57 +0000

    Implement dashboard layout and add new todo component

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { InputComponent } from "../components/core/input/input.component";
 
 @Component({
-  selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  selector: "app-dashboard",
+  imports: [InputComponent],
+  templateUrl: "./dashboard.component.html",
+  styleUrl: "./dashboard.component.css",
 })
 export class DashboardComponent {
-
+  onSearchChange = (value: string) => {
+    console.log("Search value:", value);
+  };
 }
